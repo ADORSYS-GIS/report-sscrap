aea04ba043205f9f89afb1b7ee79dd894ba2b0f1
Stephane Segning <selastlambou@gmail.com>
2023-11-15 17:56:04 +0100

    chore(layout): added basic layout for the UI

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index")
 def index():
-	return render_template("index.html")
+	return redirect(url_for("input"))
+
+@app.route("/input")
+def input():
+	return render_template("input.html")
+
+@app.route("/results")
+def results():
+	return render_template("results.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
