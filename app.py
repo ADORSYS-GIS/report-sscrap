402780003737390f1ca445874fec9000b1455588
Bansikah <tandapnoelbansikah@gmail.com>
2023-11-17 19:55:02 +0100

    feature(backend) setup backend to receive urls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,27 @@ def index():
 @app.route("/input")
 def input():
 	return render_template("input.html")
+
+#setting up backend to receive urls
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    urls = request.form.getlist('urls')
+    validated_urls = validate_urls(urls)
+    
+    if validated_urls:
+        return jsonify({'message': 'Scraping in progress...', 'validated_urls': validated_urls}), 200
+    else:
+        return jsonify({'error': 'Invalid URLs provided.'}), 400
+
+def validate_urls(urls):
+    validated_urls = []
+    
+    for url in urls:
+        if url.startswith('http://') or url.startswith('https://'):
+            validated_urls.append(url)
+    
+    return validated_urls
+
 
 @app.route("/results")
 def results():
