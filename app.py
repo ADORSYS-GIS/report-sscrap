1960dc117ac7dd4139bd40b9d85282b152471634
forkimenjeckayang <forkimenjeckayang@gmail.com>
2023-11-18 16:15:13 +0100

    Modification(app.py: added a new route to scrape extracted data and store in csv format)

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
+import json
+from sscrapy import extract_and_store_csv
+import requests
 
 app = Flask(__name__)
 
@@ -11,6 +15,12 @@ def index():
 @app.route("/input")
 def input():
 	return render_template("input.html")
+
+@app.route("/sscrapy")
+def extract_csv():
+	source = requests.get('https://webscraper.io/blog').text
+	soup = BeautifulSoup(source, 'lxml')
+	return extract_and_store_csv(soup)	
 
 @app.route("/results")
 def results():
