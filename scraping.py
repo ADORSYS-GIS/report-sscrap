c858185179d3288dbd8e056de6e3b4cc3ed493ff
nkwajudetambe <nkwajudetambe@gmail.com>
2023-11-24 04:28:34 +0100

    changed scrapping logic so that it provides user with 2 choices to either scrapp images or number of text from url

--- scraping.py
+++ scraping.py
@@ -1,26 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import RequestException
-from urllib.error import HTTPError
-from urllib.error import URLError
+from urllib.error import HTTPError, URLError
 
-def scrape_website(url):
+def scrape_website(url, choice):
+    total_characters = 0
+    extracted_data = []
+
     try:
         response = requests.get(url)
-        response.raise_for_status() 
+        response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
-        extracted_data = []
-        for element in soup.find_all('p'):
-            extracted_data.append(element.text)
-        return extracted_data
-    
+
+        if choice == '1':  # User chose to scrape text
+            for element in soup.find_all('p'):
+                extracted_data.append(element.text)
+                total_characters += len(element.text)
+        elif choice == '2':  # User chose to scrape images
+            for img in soup.find_all('img', src=True):
+                extracted_data.append(img['src'])
+        else:
+            print("Invalid choice. Please enter '1' or '2'.")
+            return None
+
+        return extracted_data, total_characters
+
     except requests.HTTPError as e:
         print("HTTP error occurred:")
         print(e)
-
     except requests.RequestException as e:
         print("An error occurred:")
         print(e)
-
     except Exception as e:
         print(f"Error scraping website: {url}")
+        
