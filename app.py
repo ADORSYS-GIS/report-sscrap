0a19aa0e48857435e758bc395f87931caa206fe3
Elwizzy12 <chiahans123@gmail.com>
2023-11-25 09:53:23 +0100

    read and save data

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 import json
-from scraping import extract_and_store_csv
+from resource.scraping import extract_and_store_csv
 import requests
 import csv
 import json
@@ -18,91 +18,6 @@ def index():
 @app.route("/input")
 def input():
     return render_template("input.html")
-
-@app.route("/sscrapy")
-def extract_csv():
-	source = requests.get('https://webscraper.io/blog').text
-	soup = BeautifulSoup(source, 'lxml')
-	return extract_and_store_csv(soup)	
-
-#setting up backend to receive urls
-@app.route('/save_url', methods=['POST'])
-def scrape():
-    urls = request.form.getlist('urls')
-    validated_urls = validate_urls(urls)
-    
-    if validated_urls:
-        return jsonify({'message': 'Scraping in progress...', 'validated_urls': validated_urls}), 200
-    else:
-        return jsonify({'error': 'Invalid URLs provided.'}), 400
-
-def validate_urls(urls):
-    validated_urls = []
-    
-    for url in urls:
-        if url.startswith('http://') or url.startswith('https://'):
-            validated_urls.append(url)
-    
-    return validated_urls
-
-@app.route("/results")
-def results():
-    return render_template("results.html")
-
-@app.route('/scrape', methods=['POST'])
-def scrape():
-    url = request.form.get('url')
-    depth = int(request.form.get('depth', 1))
-    data_to_look_for = request.form.get('data_to_look_for', '')
-
-    # Scrape data from the provided URL
-    scraped_data = scrape_data(url, depth, data_to_look_for)
-
-    # Save scraped data to CSV and JSON files
-    save_to_csv(scraped_data)
-    save_to_json(scraped_data)
-
-    return render_template('results.html', data=scraped_data)
-
-def scrape_data(url, depth, data_to_look_for):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Implementing logic to extract relevant data from the BeautifulSoup object
-        scraped_data = extract_data(soup, depth, data_to_look_for)
-        return scraped_data
-    except requests.exceptions.RequestException as e:
-        print(f"Error during scraping: {e}")
-        return None
-
-def extract_data(soup, depth, data_to_look_for):
-    # Implementing logic to extract data here
-    paragraphs = soup.find_all('p')
-    scraped_data = [p.text.strip() for p in paragraphs]
-    return scraped_data[:depth]
-
-def save_to_csv(data):
-    directory = 'scraped_data'
-    os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
-
-    with open(os.path.join(directory, 'scraped_data.csv'), 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Data'])
-        for item in data:
-            writer.writerow([item])
-
-def save_to_json(data):
-    directory = 'scraped_data'
-    os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
-
-    with open(os.path.join(directory, 'scraped_data.json'), 'w', encoding='utf-8') as jsonfile:
-        json.dump(data, jsonfile, ensure_ascii=False, indent=2)
-
-@app.route('/api/start-analysis', methods = ['GET', 'POST'])
-def analysis():
-	return render_template('test.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
