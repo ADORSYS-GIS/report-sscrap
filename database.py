8f9214b98ad8f40f1d9f92274f8be824afbf10c5
nkwajudetambe <nkwajudetambe@gmail.com>
2023-11-29 12:01:47 +0100

    feature(back-end)test db operation

--- database.py
+++ database.py
@@ -16,7 +16,11 @@ def create_database_schema():
     ''')
 
     connection.commit()
+
 def store_analysis_results_in_database(results):
+    # Create the database schema if it doesn't exist
+    create_database_schema()
+
     connection = sqlite3.connect('analysis_results.db')
     cursor = connection.cursor()
 
@@ -25,7 +29,7 @@ def store_analysis_results_in_database(results):
             cursor.execute('''
                 INSERT INTO analysis_results (url, number_of_text, total_text, number_of_images)
                 VALUES (?, ?, ?, ?)
-            ''', (result['url'], len(result['text']), len(result['total_text']), len(result['images'])))
+            ''', (result['url'], result['number_of_text'], result['total_text'], result['number_of_images']))
     except sqlite3.Error as e:
         print(f"SQLite error: {e}")
     finally:
