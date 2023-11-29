7340c6b2f510b25d9bf1d701dbfb8a6c754e7f45
kouamschekina <kouamdelphine82@gmail.com>
2023-11-29 14:20:58 +0100

    rebase(local): pulling recent commits

--- static/js/chart_rendering.js
+++ static/js/chart_rendering.js
@@ -1,61 +1,55 @@
 // Retrieving chart data from query parameters
 const urlParams = new URLSearchParams(window.location.search);
-console.log(window.location.search);
-const labels = JSON.parse(decodeURIComponent(urlParams.get('labels')));
-console.log(labels);
+const labels = JSON.parse(urlParams.get('labels'));
 const text_data = JSON.parse(urlParams.get('text_data'));
-console.log(text_data)
 const no_images = JSON.parse(urlParams.get('no_images'));
-console.log(no_images)
-const images = JSON.parse(urlParams.get('images'));
-console.log(images);
 
 // Creating the chart for total number of characters
 const ctx = document.getElementById('Total_number_of_characters').getContext('2d');
 const chart1 = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: labels,
-    datasets: [{
-      label: 'Number of Text',
-      data: text_data,
-      backgroundColor: 'rgba(255, 255, 255, 1)',
-      borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Total Characters On Each Website',
+            data: text_data,
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-  }
 });
 
 // Creating the chart for images found
 const ctxn = document.getElementById('Total_number_of_images').getContext('2d');
 const chart2 = new Chart(ctxn, {
-  type: 'bar',
-  data: {
-    labels: labels,
-    datasets: [{
-      label: 'Number of Images',
-      data: no_images,
-      backgroundColor: 'rgba(255, 255, 255, 1)',
-      borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Images Found On Each Website',
+            data: no_images,
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-  }
 });
 
 // Passing image data to script
