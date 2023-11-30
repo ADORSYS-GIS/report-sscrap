ee0cf26e321bbe24d1e97e9aebd6f47a26b0b217
ValantineSuh <148695389+ValantineSuh@users.noreply.github.com>
2023-11-30 14:27:17 +0100

    feature(backend)added intergration with analysis script

--- app.py
+++ app.py
@@ -95,6 +95,10 @@ def save_to_json(data):
 @app.route('/api/start-analysis', methods = ['GET', 'POST'])
 def analysis():
 	return render_template('test.html')
+#added and reconfigure functionality to coonect database to the analysis script 
+        store_analysis_results_in_database(scraped_data)
+    flash('Scraping and saving to database successful!', 'success')
+    return render_template('results.html', data=scraped_data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
